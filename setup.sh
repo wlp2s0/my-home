a78fda689d3e98d9b40a8067596e2185e29856c8
Mattia Campagna <mattia.campagna@webcom-tlc.com>
2023-02-03 17:02:37 +0100

    setup wip

--- setup.sh
+++ setup.sh
@@ -4,16 +4,27 @@ sudo apt-get install -y \
     ca-certificates \
     curl \
     software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo apt-key fingerprint 0EBFCD88
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
+
+sudo apt-get install -y \
+    ca-certificates \
+    curl \
+    gnupg \
+    curl \
+    lsb-release
+
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+
 sudo apt-get update
-sudo apt-get install -y docker-ce
-sudo docker run hello-world
-# Linux post-install
+
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+# Docker post-install
 sudo groupadd docker
 sudo usermod -aG docker $USER
 sudo systemctl enable docker
