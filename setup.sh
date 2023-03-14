62866517f9cfe15a5c12cb07531a199e6c7573ed
Mattia Campagna <me@mattiacampagna.com>
2023-03-14 13:02:08 +0000

    Added pihole

--- setup.sh
+++ setup.sh
@@ -31,6 +31,9 @@
  docker network create traefik
 
 # Create directories
+## filebrowser
+mkdir -p filebrowser/database
+touch filebrowser/database/filebrowser.db
 ## traeafik
 mkdir -p traefik/letsencrypt
 touch traefik/letsencrypt/acme.json
@@ -38,12 +41,15 @@ chmod 600 traefik/letsencrypt/acme.json
 ## jellyfin shared folders 
 mkdir -p shared/movies
 mkdir -p shared/tvseries
-## filebrowser
-mkdir -p filebrowser/database
-touch filebrowser/database/filebrowser.db
+
+# Disable systemd-resolved caching DNS stub resolver.
+# Reference: https://github.com/pi-hole/docker-pi-hole#installing-on-ubuntu
+sudo sed -r -i.orig 's/#?DNSStubListener=yes/DNSStubListener=no/g' /etc/systemd/resolved.conf
+sudo sh -c 'rm /etc/resolv.conf && ln -s /run/systemd/resolve/resolv.conf /etc/resolv.conf'
+systemctl restart systemd-resolved
 
 # Start all services
-declare -a services=("traefik" "heimdall" "jellyfin" "nextcloud" "transmission" "filebrowser")
+declare -a services=("traefik" "heimdall" "jellyfin" "nextcloud" "transmission" "filebrowser" "pihole")
 
 for service in "${services[@]}"
 do
