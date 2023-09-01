410091b24b651fb55590282d5ee66cd70909a55f
Mattia Campagna <me@mattiacampagna.com>
2023-09-01 14:17:38 +0000

    wip

--- setup.sh
+++ setup.sh
@@ -1,34 +1,34 @@
 #!/bin/bash
 
- sudo apt-get install -y \
-     apt-transport-https \
-     ca-certificates \
-     curl \
-     software-properties-common
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
 
- sudo apt-get install -y \
-     ca-certificates \
-     curl \
-     gnupg \
-     curl \
-     lsb-release
+sudo apt-get install -y \
+    ca-certificates \
+    curl \
+    gnupg \
+    curl \
+    lsb-release
 
- sudo mkdir -p /etc/apt/keyrings
- curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
- echo \
-   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
- sudo apt-get update
+sudo apt-get update
 
- sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
 # Docker post-install
- sudo groupadd docker
- sudo usermod -aG docker $USER
- sudo systemctl enable docker
- docker network create traefik
+sudo groupadd docker
+sudo usermod -aG docker $USER
+sudo systemctl enable docker
+docker network create traefik
 
 # Create directories
 ## filebrowser
@@ -53,9 +53,7 @@ declare -a services=("traefik" "heimdall" "jellyfin" "nextcloud" "transmission" 
 
 for service in "${services[@]}"
 do
-
- cd "$service"
- docker compose up -d
- cd .. 
-
+  cd "$service"
+  docker compose up -d
+  cd .. 
 done
